8937b0c2ad2faeaad4d029dd693bf694afffcea5
Eugen Don <eugendon87@gmail.com>
2017-03-31 05:58:15 +0200

    Various fixes and improvements

--- auth_admin_passkey/__manifest__.py
+++ auth_admin_passkey/__manifest__.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Admin Passkey module for OpenERP
-#    Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Authentification - Admin Passkey',
-    'version': '10.0.2.1.1',
+    'version': '10.0.1.0.0',
     'category': 'base',
     'author': "GRAP,Odoo Community Association (OCA)",
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
     'depends': [
         'mail',
+        'base_setup',
     ],
     'data': [
         'data/ir_config_parameter.xml',
